b8e5cba495427c0e446ab8184cfb010d99027134
Mangaldip <you@example.com>
2020-12-22 15:01:31 +0530

    color code add and tab bar changes in provider app

--- app/src/main/java/com/dev/pigeonproviderapp/ProviderDetails.java
+++ app/src/main/java/com/dev/pigeonproviderapp/ProviderDetails.java
@@ -1,13 +1,27 @@
 package com.dev.pigeonproviderapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.dev.pigeonproviderapp.activity.BaseActivity;
 
 public class ProviderDetails extends BaseActivity {
-
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_details);
+
+        btnSubmit=findViewById(R.id.btn_providersubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ProviderDetails.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
