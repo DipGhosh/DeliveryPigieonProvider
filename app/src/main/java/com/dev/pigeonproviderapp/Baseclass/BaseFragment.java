c3638e52e012718c59fc919a6d4c17661954e4b7
Mangaldip <you@example.com>
2021-01-15 21:56:22 +0530

    Change directory name Base class

---
+++ app/src/main/java/com/dev/pigeonproviderapp/Baseclass/BaseFragment.java
@@ -0,0 +1,12 @@
+package com.dev.pigeonproviderapp.Baseclass;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public abstract class BaseFragment extends Fragment {
+
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
--- app/src/main/java/com/dev/pigeonproviderapp/MainActivity.java
+++ app/src/main/java/com/dev/pigeonproviderapp/MainActivity.java
@@ -1,24 +1,8 @@
 package com.dev.pigeonproviderapp;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import com.dev.pigeonproviderapp.Fragment.OrdersFrag;
-import com.dev.pigeonproviderapp.Fragment.ProfileFrag;
-import com.dev.pigeonproviderapp.Fragment.SupportFrag;
-import com.dev.pigeonproviderapp.activity.BaseActivity;
-import com.google.android.material.tabs.TabLayout;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.dev.pigeonproviderapp.Baseclass.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
